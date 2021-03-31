e0185f3f0ae71723f15968e4bf418ae0e8c97e46
kalong0126 <32868721+kalong0126@users.noreply.github.com>
2021-03-31 13:19:15 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "kalong0126/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
