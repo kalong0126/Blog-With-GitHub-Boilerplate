5cd0edb58917169384177c11c1cd3f339bef098e
kalong0126 <32868721+kalong0126@users.noreply.github.com>
2021-03-31 16:42:27 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,13 +20,13 @@ enable_jsdelivr = {
 }
 category_by_folder = "True"
 # 站点设置
-site_name = "陈伟的个人博客"
+site_name = "散人陈的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "chinwer"
 email = "kalong126@gmail.com"
 author_homepage = "https://chinwer.top"
-description = "只坚持一种正义。我的正义。"
+description = "存档专用"
 key_words = ['Maverick', 'chinwer', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
